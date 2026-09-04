6063f6ffe55da934f65595726bec563aa4fdc7be
Imran Gul <gulimran735@gmail.com>
2026-09-05 03:48:38 +0500

    style: load translator navbar styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import "./client-revisions.css";
 import "./conjugator-redesign.css";
 import "./translator-consistency.css";
+import "./translator-navbar.css";
 import "./top-promo-bar.css";
 
 const tunFavicon = "https://tunapp.com/wp-content/uploads/2020/09/cropped-Tun_Site-Icon-180x180.png";
